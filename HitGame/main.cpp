b56b6fbecb7135ecc4e56ee54adcf8277903c7e5
devanshugarg <devanshugarg@DEVANSHUGARG>
2020-11-09 17:58:12 +0530

    Setup

---
+++ HitGame/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main() {
+
+	std::cout << "Hello" << std::endl;
+
+	system("pause");
+
+	return 0;
+}
